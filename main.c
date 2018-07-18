236a57ff921b6129be0de57a48f2d0011d387edb
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-07-18 23:59:11 +0200

    Documentation

--- main.c
+++ main.c
@@ -13,19 +13,28 @@ void *safe_malloc(size_t size) {
 }
 
 
+int safe_return_example() {
+  raii_with(int *someint, malloc(sizeof(int)), free) {
+    if (someint == NULL){
+      break; // immediately relinquishes this resource and continues after matching `}`.
+    }
 
-int foo() {
-  raii_with(int *foo, safe_malloc(sizeof(int)), free){
-    *foo = 40;
-    raii_with(int *bar, safe_malloc(sizeof(int)), free){
-      *bar = *foo + 2;
-      printf("Bar %d!\n", *bar);
-      printf("Foo! %d\n", *foo);
-      int res = *bar;
+    *someint = 40;
+    raii_with(int *otherint, safe_malloc(sizeof(int)), free) {
+      *otherint = *someint + 2;
+      printf("someint: %d!\n", *otherint);
+      printf("otherint: %d!\n", *someint);
+
+      // Note we don't call `safe_return *bar` directly,
+      // since that would be a wild pointer dereference.
+      int res = *otherint;
       safe_return res;
     }
   }
+
+  return -1; // default value
 }
+
 
 int main() {
 
@@ -36,7 +45,7 @@ int main() {
     printf("%s\n", __func__);
   }
 
-  int res = foo();
+  int res = safe_return_example();
   printf("res: %d\n", res);
 
 printf("Test\n");
--- raii/raii.h
+++ raii/raii.h
@@ -32,39 +32,67 @@ struct raii_lifetime_list_t {
  **/
 struct raii_lifetime_list_t *raii_lifetime_list;
 
+/**
+ * Macro to glue together two parts; mostly used to make unique labels and variable names
+ * based on the current line number, by using __LINE__ as `y`
+ *
+ * The extra 'impl' redirect is there to make sure the arguments are macro-expanded before gluing them.
+ */
 #define raii_glueimpl(x, y) x ## y
 #define raii_glue(x, y) raii_glueimpl(x, y)
 
-#define safe_return                                             \
-  while (raii_lifetime_list != NULL) {                     \
-    destruct_raii_lifetime(raii_lifetime_list->elem);  \
-    raii_lifetime_list = raii_lifetime_list->next;   \
-  }                                                             \
-  return                                                        \
+/**
+ * Used to quickly return (either a value or void) from a (deeply nested) `raii_with` statement.
+ * !Important! Do not return the contents of a resource acquired from `raii_with` directly, but store these in a separate variable first!
+ * This is necessary because the resources will first be destructed, so otherwise you end up referencing something that no longer exists.
+ * (like dereferencing a NULL-pointer)
+ */
+#define safe_return                                   \
+  while (raii_lifetime_list != NULL) {                \
+    destruct_raii_lifetime(raii_lifetime_list->elem); \
+    raii_lifetime_list = raii_lifetime_list->next;    \
+  }                                                   \
+  return                                              \
 
-#define raii_with(var_decl, init, destr)                                   \
-  while(1)                                                              \
+/**
+ * Custom Control Structure Macro to provide Resource Acquisition Is Initialization (and Resource Relinquishment is Destruction).
+ *
+ * Use this to run a block of code with `var_decl` initialized to `init`, where at the end of the block (or at an earlier `safe_return`),
+ * the passed `destr`-function will automatically be called with the given resource.
+ *
+ * Gotcha's:
+ * 1. Do not use `return` from within `raii_with`, but only `safe_return`, because otherwise the destructors will not be run.
+ * 2. Do not perform pointer-swaps with `var_decl`; the destructor will still be run on the original structure, because `raii` keeps its own reference to the resource.
+ */
+#define raii_with(var_decl, init, destr)                                \
+  while(1) /* i.c.m. break on l.4, so we can jump past the user-supplied block */ \
     if(0)                                                               \
-    raii_glue(__using_finished, __LINE__):                         \
+    raii_glue(__raii_with_finished, __LINE__):                              \
       break;                                                            \
     else                                                                \
+      /* initialize _tmp lifetime list elem so replacement `raii_lifetime_list` can have previous one as tail. */ \
       for(struct raii_lifetime_list_t _tmp = {.elem.resource = init, .elem.destructor = destr, .next = raii_lifetime_list};;) \
+        /* initialize user-supplied variable name */                    \
         for(var_decl = _tmp.elem.resource;;)                            \
           if (1) {                                                      \
-            _tmp.next = raii_lifetime_list;                        \
-            goto raii_glue(__using_setup, __LINE__);               \
+            /* Fill `_tmp`'s tail before `raii_lifetime_list` is shadowed */ \
+            _tmp.next = raii_lifetime_list;                             \
+            goto raii_glue(__raii_with_setup, __LINE__);                    \
           } else                                                        \
-          raii_glue(__using_setup, __LINE__):                      \
+          raii_glue(__raii_with_setup, __LINE__):                           \
+            /* Shadow `raii_lifetime_list` with inner version */        \
             for(struct raii_lifetime_list_t *raii_lifetime_list = &_tmp;;) \
               if(1){                                                    \
-                goto raii_glue(__using_body, __LINE__);            \
+                goto raii_glue(__raii_with_body, __LINE__);                 \
               } else                                                    \
-                while (1)                                               \
-                  if (1){                                               \
-                    destruct_raii_lifetime(raii_lifetime_list->elem); \
-                    goto raii_glue(__using_finished, __LINE__);    \
-                  } else                                                \
-                  raii_glue(__using_body, __LINE__):
+                while (1) /* so break works as expected */              \
+                  while (1) /*so continue works as expected */          \
+                    if (1){                                             \
+                      /*after the else-block (or break or continue), destruct and quit*/ \
+                      destruct_raii_lifetime(raii_lifetime_list->elem); \
+                      goto raii_glue(__raii_with_finished, __LINE__);       \
+                    } else                                              \
+                    raii_glue(__raii_with_body, __LINE__):
 
 
 #endif // RAII_WITH_H
